4048494e3869ab67e5a3ca250f7d3cc6a1236739
shulamitmorjossef <shulamitmorjossef@gmail.com>
2025-05-14 19:22:29 +0300

    maps server

--- backend/routes/safePlaceRoutes.js
+++ backend/routes/safePlaceRoutes.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 const safePlaceController = require('../controllers/safePlaceController');
 
+router.get('/', safePlaceController.getAllSafePlaces);
+
 
 module.exports = router;
